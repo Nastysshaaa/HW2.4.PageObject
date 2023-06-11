d180356e59adb8de50d0ce9093499f46d5afccc6
Anastasiia <anastasia994@yandex.ru>
2023-06-11 11:29:23 +0500

    Changes files

---
+++ src/test/java/page/DashboardPage.java
@@ -0,0 +1,50 @@
+package page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+    private ElementsCollection cards = $$(".list__item div");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public void Dashboard() {
+       heading.shouldBe(visible);
+    }
+
+    public int getFirstCardBalance() {
+        val text = cards.first().text(); //getValue = val
+        int balance = extractBalance(text);
+        return balance;
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+    public int getSecondCardBalance() {
+        val text = cards.get(1).text();
+        int balance = extractBalance(text);
+        return balance;
+    }
+    public TransferPage depositFirstCard() {
+        $$("[data-test-id=action-deposit]").get(0).click();
+        return new TransferPage();
+    }
+    public TransferPage depositSecondCard() {
+        $$("[data-test-id=action-deposit]").get(1).click();
+        return new TransferPage();
+    }
+
+}
---
+++ src/test/java/page/VerificationPage.java
@@ -0,0 +1,16 @@
+package page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input"); //поиск элементов вынесен в отдельные поля и в методе используются именно эти поля
+    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible); //определен конструктор и проверяем что есть поле ввода кода, Selenide библиотека по умолчанию ждет 4 секунды, страница должна открыться
+    }
+
+}
